0f5377c971ed0ca0050c8bb30bab4541f227f5dc
Arthurin <6535135+Arthurin@users.noreply.github.com>
2025-07-15 21:53:48 +0200

    end of chapter 11

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,9 +28,9 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split("T")[0];
 
   await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
 
   revalidatePath("/dashboard/invoices");
 
@@ -49,10 +49,10 @@ export async function updateInvoice(id: string, formData: FormData) {
   const amountInCents = amount * 100;
 
   await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
