7733a43fe91433f2daa6cf5667ecc057b6286bb4
Arthurin <6535135+Arthurin@users.noreply.github.com>
2025-07-16 19:53:49 +0200

    end of chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,9 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -123,4 +126,23 @@ export async function deleteInvoice(id: string, formData: FormData) {
   `;
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
